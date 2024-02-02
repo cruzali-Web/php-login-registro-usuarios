852f09c67465e9afc9af7e0e8927120ab4cb6cbe
ali.cruz@me.com <ali.cruz@me.com>
2024-02-02 07:55:05 -0600

    Update config

--- configuration.php
+++ configuration.php
@@ -1,7 +1,9 @@
 <?php
     // Detalles para la conexión a MySQL
-    $server     = "localhost:3311";
+    //$server     = "localhost:3311";
+    $server     = "192.168.1.161:3302"; // H
     $user       = "root";
-    $password   = "";
+    //$password   = "";
+    $password   = "p4ssw0rd"; // H
     $db         = "Cursos_1";
 ?>
